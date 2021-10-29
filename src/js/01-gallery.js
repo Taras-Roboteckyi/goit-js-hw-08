b22c50d7eec09ea0e500c1fc7a54047797d1a1cb
Taras-Roboteckyi <rtvr@ukr.net>
2021-10-29 23:13:20 +0300

    Завдання №2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryList = document.querySelector('.gallery');
+
+//Створення і рендер розмітки 
+
+function renderImages() {
+    const markup = galleryItems
+        .map(
+            ({preview, original, description}) =>
+    
+          `<div class="list__item">
+            <a class="gallery__item" href="${original}">
+           <img class="gallery__image"
+           src="${preview}" 
+           alt="${description}" />
+                </a>
+           </div>`
+
+    )
+    .join('');
+    galleryList.insertAdjacentHTML('beforeend', markup);
+    console.log(galleryList);
+}
+renderImages();
+
+
+// Застосування бібліотеки SimpleLightbox
+
+new SimpleLightbox('.gallery .gallery__item', { captionsData:'alt', captionDelay:250});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+   
+     import throttle from 'lodash.throttle'
+
+    const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
+    const KEY = "videoplayer-current-time"
+    
+   player.on('timeupdate', throttle(currentTimeSeconds, 1000));
+   
+  function currentTimeSeconds() {
+    player.getCurrentTime()
+        .then(function (seconds) {
+            localStorage.setItem(KEY, seconds)
+        }).catch(function (error) {
+            console.log("an error occurred", error)
+        });
+}
+
+
+player.setCurrentTime(localStorage.getItem(KEY))
+  .catch(function(error) {
+   console.log('RangeError', error)
+});
+
 
